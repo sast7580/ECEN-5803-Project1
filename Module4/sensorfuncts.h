5511dc964ad7e552a35cb2222847a304971f9390
sast7580 <46766721+sast7580@users.noreply.github.com>
2019-02-26 23:15:32 -0700

    Create sensorfuncts.h

---
+++ Module4/sensorfuncts.h
@@ -0,0 +1,10 @@
+#include "mbed.h"
+
+void adc_configure_opt0();
+void adc_configure_opt1();
+float adc_read_8bit();
+float adc_read_16bit(int sel);
+int adc_calibrate();
+float spi_lcd();
+int freqPulse();
+float flowPulse();
